bd0ac6ca85952342b467ece2afd158bdf366b5ed
Larissa Nunes <64762578+larissa-NUF@users.noreply.github.com>
2021-11-12 12:35:53 -0300

    projeto custos

---
+++ custosArquivos/src/App.js
@@ -0,0 +1,37 @@
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import Home from './components/pages/Home';
+import Sobre from './components/pages/Sobre';
+import Contato from './components/pages/Contato';
+import NovoProjeto from './components/pages/NovoProjeto';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/sobre">Sobre</Link>
+        <Link to="/novoprojeto">Novo Projeto</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/sobre">
+          <Sobre />
+        </Route>
+        <Route exact path="/contato">
+          <Contato />
+        </Route>
+        <Route exact path="/novoprojeto">
+          <NovoProjeto />
+        </Route>
+      </Switch>
+      <p>Footer</p>
+    </Router>
+    
+  );
+}
+
+export default App;
